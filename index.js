0056c4e1c02544451b642a5bec8b34075032adcd
Eyan-Green <ian.green736@btinternet.com>
2018-08-08 15:19:33 +0100

    fix blank text palindrome error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 module.exports = Phrase; 
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
@@ -22,11 +23,7 @@ function Phrase(content) {
 	}
 // Returns true for a palindrome, false otherwise.
 	this.palindrome = function palindrome() {
-    	if (this.letters()) {
-      		return this.processedContent() === this.processedContent().reverse();
-    	} else {
-      	return false;
-    	}
+		return this.processedContent() === this.processedContent().reverse();
 	}
 }
 // Defines a TranslatedPhrase object.
@@ -40,3 +37,26 @@ function TranslatedPhrase(content, translation) {
 }
 
 TranslatedPhrase.prototype = new Phrase();
+},{}],2:[function(require,module,exports){
+let Phrase = require("ian-palindrome");
+
+function palindromeTester(event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is a not a palindrome.`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
+},{"ian-palindrome":1}]},{},[2]);
